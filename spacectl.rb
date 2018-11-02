75b5d44d1f178dea04d1455d57d0bfa6733593ce
Martin Helmich <m.helmich@mittwald.de>
2018-11-02 09:33:08 +0100

    Bump spacectl version

--- spacectl.rb
+++ spacectl.rb
@@ -2,24 +2,19 @@ class Spacectl < Formula
   desc "CLI client for Mittwald SPACES"
   homepage "https://spaces.de"
   url "https://github.com/mittwald/spacectl.git",
-    :tag => "v1.8.0",
-    :revision => "bc684e31bc378f6af884e4b4a5da9ce5fb82748e"
+    :tag => "v1.10.0",
+    :revision => "baac43487f1aff3c20b2ce9b971f1d6e54d9781e"
   head "https://github.com/mittwald/spacectl.git"
   
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    ENV["CI_BUILD_TAG"] = "v1.8.0"
-    ENV["CI_COMMIT_SHA"] = "bc684e31bc378f6af884e4b4a5da9ce5fb82748e"
+    ENV["CI_BUILD_TAG"] = "v1.10.0"
+    ENV["CI_COMMIT_SHA"] = "baac43487f1aff3c20b2ce9b971f1d6e54d9781e"
 
-    (buildpath/"src/github.com/mittwald/spacectl").install buildpath.children
-
-    cd "src/github.com/mittwald/spacectl" do
-      system "go", "generate"
-      system "go", "build"
-      bin.install "spacectl" => "spacectl"
-    end
+    system "go", "generate"
+    system "go", "build"
+    bin.install "spacectl" => "spacectl"
   end
 
   test do
